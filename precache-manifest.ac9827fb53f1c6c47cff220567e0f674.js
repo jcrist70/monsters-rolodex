4a75d4ac6d4eded063f87ead29276a4a3a5e9f1e
jcrist70 <jcrist70@gmail.com>
2019-09-02 07:05:33 -0700

    Updates

---
+++ precache-manifest.ac9827fb53f1c6c47cff220567e0f674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abe83a38487991fba505e02e824fb29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "75d0c7e345c35b5634fe",
+    "url": "/monsters-rolodex/static/css/main.093985eb.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "75d0c7e345c35b5634fe",
+    "url": "/monsters-rolodex/static/js/main.4a3e751f.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
